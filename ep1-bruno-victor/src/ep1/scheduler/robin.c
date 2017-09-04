17ee0293a6a8587b93c59affa9725bb70f047846
Bruno Scholl <bruno.scholl@usp.br>
2017-09-04 17:11:18 -0300

    [ep1] started the round robin , will catch tomorrow

--- ep1-bruno-victor/src/ep1/scheduler/robin.c
+++ ep1-bruno-victor/src/ep1/scheduler/robin.c
@@ -1,10 +1,15 @@
 #include "ep1/scheduler/robin.h"
 #include <semaphore.h>
 #include <stdlib.h>
+#include "typedef.h"
 
 //TODO: preciso de uma linked list pra guardar a lista de processos
 //TODO: preciso que o scheduler marque o tempo
 //TODO: preciso definir quanto é um quantum
+
+//semaphore for the linked list
+sem_t ll_s;
+linked_queue ll = 0;
 
 void c_ll_insert(c_ll_item **root, process *proc)
 {
@@ -31,11 +36,34 @@ void c_ll_insert(c_ll_item **root, process *proc)
 
 void rr_init(void *sch_init)
 {
+    if (globals.extra)
+        printf("[RR ] Initializing Scheduler...\n");
+
+    // Initializes a linked list
+    c_ll = 0;
+
+    // Initializes semaphore
+    sem_init(&ll_s, 0, 1);
 }
 
 void *rr(void *sch_init)
 {
-    return 0;
+    // This is the event queue
+    ev_queue events = 0;
+
+    // Get definition
+    scheduler_def *def = (scheduler_def*)sch_init;
+
+    if (globals.extra)
+        printf("[RR ] Scheduler has started!\n");
+
+    // This is the currently running processes
+    unsigned int cpu_cnt = def->cpu_count;
+    int *free_cpu_stack = malloc(cpu_cnt * sizeof(int));
+    unsigned int *startt = malloc(cpu_cnt * sizeof(unsigned int));
+    process **running_p = malloc(cpu_cnt * sizeof(process*));
+
+
 }
 
 int rr_add_job(process *job)
