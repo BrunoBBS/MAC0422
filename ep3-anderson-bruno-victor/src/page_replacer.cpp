0c5b3caf1b9b759d836a193169eb8919cad2e470
Victor Seiji Hariki <victorseijih@gmail.com>
2017-10-19 18:09:11 -0200

    [ep3] Now with better structure

--- ep3-anderson-bruno-victor/src/page_replacer.cpp
+++ ep3-anderson-bruno-victor/src/page_replacer.cpp
@@ -3,10 +3,9 @@
 PageReplacer::PageReplacer (EP &ep, std::string name) :
     name(name)
 {
-    ep.add_page_replacer(man_id, this);
 }
 
-std::string get_name ()
+std::string PageReplacer::get_name ()
 {
     return name;
 }
